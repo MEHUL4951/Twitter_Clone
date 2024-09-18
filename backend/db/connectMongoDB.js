287516d4c2861ee03365aab1fcdb6284fff2debc
Mehul-Hadiyal <mehulhadiyal608@gmail.com>
2024-09-18 15:55:47 +0530

    First Commit

---
+++ backend/db/connectMongoDB.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
+const connectDB = async ()=>{
+    try{
+       const  DB_OPTION = {
+            user:"Admin",
+            pass:"admin@8780",
+            dbName:"twitter_clone",
+        }
+        const conn = await mongoose.connect(process.env.DATABASE_URL,DB_OPTION)
+        console.log("connect to mongodb")
+
+    }catch(err){
+        console.log(`${err.message}`)
+    }
+}
+export default connectDB
+
+
+
